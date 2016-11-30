d8b1baccc0d19de56ecd04b0bb311da1fd11b08a
zhannavoloshina <zhannavoloshina@D-69-91-130-226.dhcp4.washington.edu>
2016-11-30 12:53:43 -0800

    added firebase configurations

---
+++ src/LogInDashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import bg from './nyc.jpg';
+
+var LogInDashboard = React.createClass({
+  	render() {
+      return(
+
+
+      )
+    }
+
+
+})
+
+export default LogInDashboard;
