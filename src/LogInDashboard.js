91a2e5dd7389b0d4f3473a9a53e6a5883c1410c2
zhannavoloshina <zhanna.vol@gmail.com>
2016-12-06 09:50:06 -0800

    changes

--- src/LogInDashboard.js
+++ src/LogInDashboard.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
 import { auth } from './auth'
+import { login } from './auth'
 import bg from './nyc.jpg';
 import './App';
 import './LoginDashboard.css';
 import 'materialize-css';
+import './LogIn.css';
 
 export default class LogInDashboard extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
     auth(this.email.value, this.pw.value)
   }
+
+
+
   render () {
     return (
-      <div>
-      <div className="col-sm-6 col-sm-offset-3">
+      <div id = "page">
+        <div id = "register">
         <h1>Register</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
@@ -24,24 +29,28 @@ export default class LogInDashboard extends Component {
             <label>Password</label>
             <input type="password" className="form-control" placeholder="Password" ref={(pw) => this.pw = pw} />
           </div>
-          <button type="submit" className="btn btn-primary">Register</button>
+          <div id = "registerbutton">
+          <button type="submit"   className="btn btn-primary">Register</button>
+          </div>
         </form>
-      </div>
+        </div>
 
-      <div className="col-sm-6 col-sm-offset-3">
-        <h1>Log In</h1>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label>Email</label>
-            <input className="form-control" ref={(email) => this.email = email} placeholder="Email"/>
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input type="password" className="form-control" placeholder="Password" ref={(pw) => this.pw = pw} />
-          </div>
-          <button type="submit" className="btn btn-primary">Log In</button>
-        </form>
-      </div>
+            <div id = "login">
+                <h1>Log In</h1>
+                <form onSubmit={this.handleSubmit}>
+                  <div className="form-group">
+                    <label>Email</label>
+                    <input className="form-control" ref={(email) => this.email = email} placeholder="Email"/>
+                  </div>
+                  <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Password" ref={(pw) => this.pw = pw} />
+                  </div>
+                  <div id ="loginButton">
+                  <button type="submit" className="btn btn-primary">Log In</button>
+                  </div>
+                </form>
+                </div>
 
       </div>
     )
